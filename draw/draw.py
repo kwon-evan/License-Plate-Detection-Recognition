87018041935fa7ff2dcb643a334b179fe3d111f7
kwon-evan <koun012@naver.com>
2022-10-19 15:54:07 +0900

    color by id

--- draw/draw.py
+++ draw/draw.py
@@ -2,16 +2,16 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
 
-def plot_boxes_PIL(boxes, img, color=None, labels=None, line_thickness=None):
+def plot_boxes_PIL(boxes, img, ids, colors=None, labels=None, line_thickness=None):
     img = Image.fromarray(img)
     draw = ImageDraw.Draw(img)
     line_thickness = line_thickness or max(int(min(img.size) / 200), 2)
-    for box, label in zip(boxes, labels):
-        draw.rectangle(((box[0], box[1]), (box[2], box[3])), width=line_thickness, outline=tuple(color))  # plot
+    for box, id, label in zip(boxes, ids, labels):
+        draw.rectangle(((box[0], box[1]), (box[2], box[3])), width=line_thickness, outline=tuple(colors[id % 255]))  # plot
         if label:
             fontsize = max(round(max(img.size) / 100), 12)
             font = ImageFont.truetype("draw/Noto_Sans_KR/NotoSansKR-Regular.otf", fontsize)
             txt_width, txt_height = font.getsize(label)
-            draw.rectangle([box[0], box[1] - txt_height + 4, box[0] + txt_width, box[1]], fill=tuple(color))
+            draw.rectangle([box[0], box[1] - txt_height + 4, box[0] + txt_width, box[1]], fill=tuple(colors[id % 255]))
             draw.text((box[0] + 1, box[1] - txt_height - 1), label, fill=(255, 255, 255), font=font)
     return np.asarray(img)
--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class LicensePlateDetector(Thread):
                  yolo_weights,
                  img_size,
                  augment=False,
-                 conf_thres=0.5,
+                 conf_thres=0.4,
                  iou_thres=0.45,
                  agnostic_nms=False):
         Thread.__init__(self)
@@ -140,13 +140,13 @@ class LicensePlateDetector(Thread):
         t3 = time.time()
 
         bars['sort'].update(time=f'{(t3 - t2) * 1000:2.2f}')
-        LPs.append((im0, tracks[:, :4], tracks[:, 4]))
+        plates.append((im0, tracks[:, :4], tracks[:, 4]))
 
 
 class LicensePlateReader(Thread):
     def __init__(self, lprn_weights, device='0'):
         Thread.__init__(self)
-        self.colors = [random.randint(0, 255) for _ in range(3)]
+        self.colors = np.random.randint(255, size=(255, 3))
         self.device = select_device(device)
         self.lprn = STLPRNet().load_from_checkpoint(lprn_weights).to(self.device).eval()
         self.half = self.device.type != 'cpu'
@@ -155,24 +155,24 @@ class LicensePlateReader(Thread):
 
     def run(self):
         while True:
-            if LPs:
-                self.detect(*LPs.popleft())
+            if plates:
+                self.detect(*plates.popleft())
             else:
-                if not origin and LPs:
+                if not origin and plates:
                     print("Process Reader is Dead.")
                     return
 
             time.sleep(0.001)
 
     def detect(self, img, xyxys, ids):
-        plates = [img[y1:y2, x1:x2].astype(np.uint8) for x1, y1, x2, y2 in xyxys]
+        plate_imgs = [img[y1:y2, x1:x2].astype(np.uint8) for x1, y1, x2, y2 in xyxys]
 
         t0 = time.time()
-        preds = self.lprn.detect_imgs(plates, self.device, self.half)
+        preds = self.lprn.detect_imgs(plate_imgs, self.device, self.half)
         t1 = time.time()
 
-        if plates:
-            bars['stlprn'].update(time=f'{(t1 - t0) * 1000 / len(plates):2.2f}')
+        if plate_imgs:
+            bars['stlprn'].update(time=f'{(t1 - t0) * 1000 / len(plate_imgs):2.2f}')
 
         for id, pred in zip(ids, preds):
             while len(id_list) - 1 < id:
@@ -182,12 +182,12 @@ class LicensePlateReader(Thread):
                 id_list[id].update([pred])
 
         preds_by_id = [id_list[id].most_common()[0][0] if id_list[id] else "Unknown" for id in ids]
-        im0 = plot_boxes_PIL(xyxys, img, labels=preds_by_id, color=self.colors, line_thickness=2)
+        im0 = plot_boxes_PIL(xyxys, img, labels=preds_by_id, ids=ids, colors=self.colors, line_thickness=2)
         buffer.append(im0)
 
 
 def update_bars(bars):
-    for bar, dq in zip(bars, [origin, LPs, buffer]):
+    for bar, dq in zip(bars, [origin, plates, buffer]):
         if len(dq) / 100 > 0.8:
             _color = 'green'
         elif len(dq) / 100 > 0.2:
@@ -230,6 +230,7 @@ class VideoViewer(Thread):
         print("Process VideoViewer Ended")
         return
 
+
 def init_bars(manager):
     bar_formats = {
         'status': '{program}{fill} Stage: {stage}{fill} FPS: {fps}',
@@ -239,26 +240,34 @@ def init_bars(manager):
         'player': '{desc}{desc_pad}{percentage:3.0f}%|{bar}| {count:{len_total}d}/{total:d} [{elapsed}<{eta}]'
     }
     bars = {
-        'status': manager.status_bar(color='bold_bright_black_on_white', status_format=bar_formats['status'], program='License Plate Detection', stage='Loading', fps=f'{0.0:.2f}', position=11),
-        'video': manager.status_bar(status_format=bar_formats['file'], file=os.path.split(VIDEO_PATH)[-1], width=vr.frameWidth, height=vr.frameHeight, length=vr.frame_counts, position=9),
-        'origin': manager.counter(total=200, desc='[origin]', unit='frame', bar_format=bar_formats['counter'], position=8),
-        'yolo': manager.status_bar(status_format=bar_formats['inference'], name='yolov7', time='0.0', unit='frame', position=7),
-        'sort': manager.status_bar(status_format=bar_formats['inference'], name=' SORT ', time='0.0', unit='frame', position=6),
-        'plates': manager.counter(total=200, desc='[plates]', unit='plate', bar_format=bar_formats['counter'], position=5),
-        'stlprn': manager.status_bar(status_format=bar_formats['inference'], name='STLPRN', time='0.0', unit='plate', position=4),
-        'buffer': manager.counter(total=200, desc='[buffer]', unit='frame', bar_format=bar_formats['counter'], position=3),
+        'status': manager.status_bar(color='bold_bright_black_on_white', status_format=bar_formats['status'],
+                                     program='License Plate Detection', stage='Loading', fps=f'{0.0:.2f}', position=11),
+        'video': manager.status_bar(status_format=bar_formats['file'], file=os.path.split(VIDEO_PATH)[-1],
+                                    width=vr.frameWidth, height=vr.frameHeight, length=vr.frame_counts, position=9),
+        'origin': manager.counter(total=200, desc='[origin]', unit='frame', bar_format=bar_formats['counter'],
+                                  position=8),
+        'yolo': manager.status_bar(status_format=bar_formats['inference'], name='yolov7', time='0.0', unit='frame',
+                                   position=7),
+        'sort': manager.status_bar(status_format=bar_formats['inference'], name=' SORT ', time='0.0', unit='frame',
+                                   position=6),
+        'plates': manager.counter(total=200, desc='[plates]', unit='plate', bar_format=bar_formats['counter'],
+                                  position=5),
+        'stlprn': manager.status_bar(status_format=bar_formats['inference'], name='STLPRN', time='0.0', unit='plate',
+                                     position=4),
+        'buffer': manager.counter(total=200, desc='[buffer]', unit='frame', bar_format=bar_formats['counter'],
+                                  position=3),
         'down': manager.status_bar(status_format='   ↓', position=2),
-        'player': manager.counter(total=vr.frame_counts, desc='[  ▶   ]', unit='frame', bar_format=bar_formats['player'], color='white_on_black', position=1)
+        'player': manager.counter(total=vr.frame_counts, desc='[  ▶   ]', unit='frame',
+                                  bar_format=bar_formats['player'], color='white_on_black', position=1)
     }
     return bars
 
 
-
 if __name__ == '__main__':
-    VIDEO_PATH = 'test/video2.mp4'
+    VIDEO_PATH = 'test/20221019_154425.mp4'
 
     # Queues
-    origin, LPs, buffer, id_list = deque(), deque(), deque(), deque()
+    origin, plates, buffer, id_list = deque(), deque(), deque(), deque()
 
     # Threads
     vr = VideoReader(path=VIDEO_PATH)
