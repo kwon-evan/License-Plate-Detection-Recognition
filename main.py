50583ae25a5e06f1103eab59c68eed010f664499
kwon-evan <koun012@naver.com>
2022-10-31 15:19:17 +0900

    using yolov7 to darknet yolov3

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import os.path
-import sys
 import time
 from collections import deque, Counter
 from threading import Thread
 import cv2
-import torch
-import torch.backends.cudnn
 from numpy import random
 import numpy as np
 import enlighten
@@ -13,12 +10,7 @@ import warnings
 from STLPRNet.model.STLPRNet import STLPRNet
 from sort.sort import Sort
 from draw.draw import plot_boxes_PIL
-
-sys.path.append('./yolov7')
-from models.experimental import attempt_load
-from utils.datasets import letterbox
-from utils.general import check_img_size, non_max_suppression, scale_coords
-from utils.torch_utils import select_device
+from yolo import Yolo
 
 warnings.filterwarnings("ignore")
 
@@ -31,16 +23,18 @@ class VideoReader(Thread):
             self.cap = cv2.VideoCapture(path)
         else:
             assert "Video file not exist."
+        self.is_full = False
+
         self.frameWidth = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 영상의 넓이(가로) 프레임
         self.frameHeight = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 영상의 높이(세로) 프레임
         self.frame_size = (self.frameWidth, self.frameHeight)
         self.frame_counts = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     def run(self):
-        is_full = False
         while True:
-            is_full = self.check_buffer()
-            if not is_full:
+            self.check_buffer()
+
+            if not self.is_full:
                 if self.cap.grab():
                     ret, frame = self.cap.retrieve()
                     origin.append(frame)
@@ -52,45 +46,30 @@ class VideoReader(Thread):
             time.sleep(0.01)
 
     def check_buffer(self):
-        if len(origin) > 100:
-            return True
-        elif len(origin) < 50:
-            return False
+        max_size = 200
+
+        if len(origin) > 200 * 0.8:
+            self.is_full = True
+        elif len(origin) < 200 * 0.2:
+            self.is_full = False
 
 
 class LicensePlateDetector(Thread):
-    def __init__(self,
-                 device,
-                 yolo_weights,
-                 img_size,
-                 augment=False,
-                 conf_thres=0.25,
-                 iou_thres=0.45,
-                 agnostic_nms=False):
+    def __init__(self, root_path, batch_size=1, gpus=0):
         Thread.__init__(self)
-        self.agnostic_nms = agnostic_nms
-        self.classes = 0
-        self.iou_thres = iou_thres
-        self.conf_thres = conf_thres
-        self.augment = augment
-        self.device = select_device(device)
-        self.half = self.device.type != 'cpu'
+
+        cfg_path = root_path + 'model.cfg'
+        weight_path = root_path + 'model.weights'
+        meta_path = root_path + 'model.data'
+
+        self.yolo = Yolo(configPath=cfg_path,
+                         weightPath=weight_path,
+                         metaPath=meta_path,
+                         batch_size=batch_size,
+                         gpus=gpus)
         self.tracker = Sort(max_age=30, min_hits=3)
 
-        # Load model
-        self.yolo = attempt_load(yolo_weights, map_location=self.device).eval()  # load FP32 model
-        self.stride = int(self.yolo.stride.max())  # model stride
-        self.img_size = check_img_size(img_size, s=self.stride)
-
-        if self.half:
-            self.yolo.half()  # to FP16
-
     def run(self):
-        # Run inference
-        if self.device.type != 'cpu':
-            self.yolo(torch.zeros(1, 3, self.img_size, self.img_size).to(self.device).type_as(
-                next(self.yolo.parameters())))  # run once
-
         while True:
             if origin:
                 self.detect(origin.popleft())
@@ -101,42 +80,21 @@ class LicensePlateDetector(Thread):
             time.sleep(0.01)
 
     def detect(self, im0):
-        # Padded resize
-        img = letterbox(im0, self.img_size, stride=self.stride)[0]
-
-        # Convert
-        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-        img = np.ascontiguousarray(img)
-
-        img = torch.from_numpy(img).to(self.device)
-        img = img.half() if self.half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
+        img = im0
 
         # Inference
         t0 = time.time()
-        pred = self.yolo(img, augment=self.augment)[0]
-
-        # Apply NMS
-        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.classes,
-                                   agnostic=self.agnostic_nms)
+        pred = self.yolo.detect(img)
+        pred = np.asarray(pred)
         t1 = time.time()
         bars['yolo'].update(time=f'{(t1 - t0) * 1000:2.2f}')
 
         # Process detections
-        tracks = np.empty((0, 5))
         t2 = time.time()
-        for i, det in enumerate(pred):  # detections per image
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-                # xyxys = [list(map(int, torch.tensor(xyxy).view(1, 4).view(-1).tolist()))
-                #          for *xyxy, conf, cls in reversed(det)]
-                xyxys_conf = np.array([list(map(int, torch.tensor(xyxy).view(1, 4).view(-1).tolist())) + [conf.tolist()]
-                                       for *xyxy, conf, cls in reversed(det)])
-                tracks = self.tracker.update(xyxys_conf).astype(int)
+        if pred.any():
+            tracks = self.tracker.update(pred).astype(int)
+        else:
+            tracks = np.empty((0, 5))
         t3 = time.time()
 
         bars['sort'].update(time=f'{(t3 - t2) * 1000:2.2f}')
@@ -144,12 +102,12 @@ class LicensePlateDetector(Thread):
 
 
 class LicensePlateReader(Thread):
-    def __init__(self, stlprn_weights, device='0'):
+    def __init__(self, stlprn_weights, cuda=True):
         Thread.__init__(self)
         self.colors = np.random.randint(127, size=(255, 3))
-        self.device = select_device(device)
+        self.device = 'cuda' if cuda else 'cpu'
         self.stlprn = STLPRNet().load_from_checkpoint(stlprn_weights).to(self.device).eval()
-        self.half = self.device.type != 'cpu'
+        self.half = self.device != 'cpu'
         if self.half:
             self.stlprn.half()
 
@@ -162,7 +120,7 @@ class LicensePlateReader(Thread):
                     print("Process Reader is Dead.")
                     return
 
-            time.sleep(0.001)
+            time.sleep(0.0001)
 
     def detect(self, img, xyxys, ids):
         plate_imgs = [img[y1:y2, x1:x2].astype(np.uint8) for x1, y1, x2, y2 in xyxys]
@@ -182,6 +140,7 @@ class LicensePlateReader(Thread):
                 id_list[id].update([pred])
 
         preds_by_id = [id_list[id].most_common()[0][0] if id_list[id] else "Unknown" for id in ids]
+        print(preds_by_id)
         im0 = plot_boxes_PIL(xyxys, img, labels=preds_by_id, ids=ids, colors=self.colors, line_thickness=2)
         buffer.append(im0)
 
@@ -264,14 +223,14 @@ def init_bars(manager):
 
 
 if __name__ == '__main__':
-    VIDEO_PATH = 'test/video1.mp4'
+    VIDEO_PATH = 'test/test4.mp4'
 
     # Queues
     origin, plates, buffer, id_list = deque(), deque(), deque(), deque()
 
     # Threads
     vr = VideoReader(path=VIDEO_PATH)
-    lpd = LicensePlateDetector(device='0', yolo_weights='weights/yolov7-best.pt', img_size=480)
+    lpd = LicensePlateDetector(root_path='plate/')
     lpr = LicensePlateReader(stlprn_weights='weights/stlprn-best.pt')
     vv = VideoViewer(fps=30)
     vr.daemon, lpd.daemon, lpr.daemon, vv.daemon = True, True, True, True
